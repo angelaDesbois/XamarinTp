891da1622c48ad884e8a841431845b46013cd825
andrea sparow <andrearaiatea@gmail.com>
2020-09-30 11:00:38 +0200

    tp3 presque fini

--- XamarinTp/XamarinTp/MainPage.xaml.cs
+++ XamarinTp/XamarinTp/MainPage.xaml.cs
@@ -8,11 +8,45 @@ using Xamarin.Forms;
 
 namespace XamarinTp
 {
+    [DesignTimeVisible(false)]
     public partial class MainPage : ContentPage
     {
         public MainPage()
         {
             InitializeComponent();
+            
+        }
+
+        private void connexion_Clicked(object sender, EventArgs e)
+        {
+            Console.WriteLine("connection is clicked");
+            String id = this.id.Text;
+            String password = this.password.Text;
+            Boolean seSouvenir = this.seSouvenir.IsToggled;
+
+            this.erreur.IsVisible = false;
+
+            if(id == null || string.IsNullOrEmpty(id) || id.Length < 3)
+            {
+                this.erreur.Text = "Entrer un id d au moins 3 caractères";
+                this.erreur.IsVisible = true;
+                return;
+            }
+
+            if (password == null || string.IsNullOrEmpty(password) || password.Length < 6)
+            {
+                this.erreur.Text = "Entrer un password d au moins 6 caractères";
+                this.erreur.IsVisible = true;
+                return;
+            }
+
+            if (this.seSouvenir.IsToggled)
+            {
+                DisplayAlert("Info", "Vous serez connecté automatiquement au prochain lancement", "Compris");
+            }
+
+            this.form.IsVisible = false;
+            this.tweet.IsVisible = true;
         }
     }
 }
