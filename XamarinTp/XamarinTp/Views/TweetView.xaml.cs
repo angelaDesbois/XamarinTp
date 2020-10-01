08fb17740fb62d2f7d256bbb9e088d7c3ccd8a4c
andrea sparow <andrearaiatea@gmail.com>
2020-10-01 14:26:31 +0200

    tpjeudi aprem pas fini

--- XamarinTp/XamarinTp/Views/TweetView.xaml.cs
+++ XamarinTp/XamarinTp/Views/TweetView.xaml.cs
@@ -1,18 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Text;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 using XamarinTp.models;
+using XamarinTp.services;
 
 namespace XamarinTp.Views
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class TweetView : ContentView
     {
+       // private ObservableCollection<Tweet> tweets = new ObservableCollection<Tweet>();
+       // private ITwitterService twitterService = new TwitterService();
         public TweetView()
         {
             InitializeComponent();
+
+         //   foreach(var item in twitterService.getTweets())
+         //   {
+          //      tweets.Add(item);
+         //   }
+             
         }
 
         public void LoadData(Tweet tweet)
